8854234ba30dd12eeabfdd6455d90f5809c335b0
Toan Le <toanlee.dev@gmail.com>
2023-03-20 20:30:18 +0700

    init project

---
+++ app/javascript/components/Home.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => <div className='hello'>Hello world</div>;
---
+++ app/javascript/components/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+document.addEventListener('turbo:load', () => {
+  const root = createRoot(document.getElementById('root'));
+  root.render(<App />);
+});
